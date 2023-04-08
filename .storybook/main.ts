3ec8ecc380f3b4b46d6fa8e670484cf07556a128
Brandon Lenz <brandonalenz@gmail.com>
2023-04-07 21:24:34 -0400

    Add and configure USWDS

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,14 @@
 import type { StorybookConfig } from '@storybook/react-webpack5'
 import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin'
 
+const path = require('path')
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    {
-      name: '@storybook/addon-styling',
-      options: {
-        sass: {
-          implementation: require('sass'),
-        },
-      },
-    },
   ],
   framework: {
     name: '@storybook/react-webpack5',
@@ -25,6 +19,29 @@ const config: StorybookConfig = {
   },
   webpackFinal: async (config, { configType }) => {
     config.resolve.plugins = [new TsconfigPathsPlugin()]
+
+    config.module.rules.push({
+      test: /\.(sa|sc|c)ss$/,
+      exclude: /\.module\.(sa|sc|c)ss$/i,
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true,
+            sassOptions: {
+              includePaths: [
+                './node_modules/@uswds',
+                './node_modules/@uswds/uswds/packages',
+              ],
+            },
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '../'),
+    })
+
     return config
   },
 }
