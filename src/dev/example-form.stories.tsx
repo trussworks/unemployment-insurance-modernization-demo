52ed05c91b0a6904aae64e73b3cb0b71d175255d
Joe Kleinschmidt <joekleinschmidt@truss.works>
2023-04-10 13:37:35 -0500

    Add yup and simple schema for example

--- src/dev/example-form.stories.tsx
+++ src/dev/example-form.stories.tsx
@@ -1,9 +1,24 @@
 import { Meta, StoryObj } from '@storybook/react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { ChangeEventHandler } from 'react'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 
 const formLibraryPreferenceOptions = ['formik', 'reactHookForm'] as const
 type FormLibraryPreferenceOption = (typeof formLibraryPreferenceOptions)[number]
+
+const schema = yup
+  .object({
+    formLibraryPreference: yup
+      .string()
+      .oneOf([...formLibraryPreferenceOptions])
+      .required(),
+    whyIsFormikBad: yup.string().when('formLibraryPreference', {
+      is: 'reactHookForm',
+      then: (schema) => schema.required(),
+    }),
+  })
+  .required()
 
 type ExampleFieldValues = {
   formLibraryPreference?: FormLibraryPreferenceOption
@@ -14,10 +29,16 @@ const defaultValues: ExampleFieldValues = {
   whyIsFormikBad: undefined,
 }
 const ExampleForm = () => {
-  const { register, handleSubmit, watch, setValue, formState } =
-    useForm<ExampleFieldValues>({
-      defaultValues,
-    })
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm<ExampleFieldValues>({
+    defaultValues,
+    resolver: yupResolver(schema),
+  })
 
   const formLibraryPreference = watch('formLibraryPreference')
 
@@ -38,6 +59,9 @@ const ExampleForm = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <fieldset>
         <legend>Which form Library is better?</legend>
+        <span className="usa-error-message" role="alert">
+          {errors.formLibraryPreference?.message}
+        </span>
         <div>
           <input
             {...remainingProps}
@@ -68,6 +92,9 @@ const ExampleForm = () => {
         <>
           <div>
             <label htmlFor="whyIsFormikBad">Why is Formik bad?</label>
+            <span className="usa-error-message" role="alert">
+              {errors.whyIsFormikBad?.message}
+            </span>
             <input {...register('whyIsFormikBad')} />
           </div>
           <br />
