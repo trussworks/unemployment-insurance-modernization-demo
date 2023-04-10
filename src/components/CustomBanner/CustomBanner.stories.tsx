78f2dac008059826e5467e0dcde63cb4962191eb
Brandon Lenz <brandonalenz@gmail.com>
2023-04-10 14:13:10 -0400

    Install and set up React Hook Form (#20)
    * Update gitignore
    
    * Add uswds dependencies
    
    * Add ts baseUrl support for storybook
    
    * Add and configure USWDS
    
    * Add i18n
    
    * Missing newline
    
    * Delete storybook examples
    
    * Match story name to title so that it doesn't nest
    
    * Add React Hook Form example
    
    * Fixes:
      - Unused import
      - incorrect label-id mapping

--- src/components/CustomBanner/CustomBanner.stories.tsx
+++ src/components/CustomBanner/CustomBanner.stories.tsx
@@ -3,11 +3,11 @@ import { Meta, StoryObj } from '@storybook/react'
 import { CustomBanner } from 'components/CustomBanner/CustomBanner'
 
 const meta = {
-  title: 'Components/Custom Banner',
+  title: 'Components/Banner',
   component: CustomBanner,
 } satisfies Meta<typeof CustomBanner>
 
 export default meta
 type Story = StoryObj<typeof CustomBanner>
 
-export const Default: Story = { args: {} }
+export const Banner: Story = {}
