b74e0074ac78d8c37597be667213ef39cf3c3c45
Joe Kleinschmidt <joekleinschmidt@truss.works>
2023-04-11 15:15:36 -0500

    Add util for ssn format

--- src/pages/ssn-page.stories.tsx
+++ src/pages/ssn-page.stories.tsx
@@ -1,11 +1,13 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { Button, ErrorMessage, Label, TextInput } from '@trussworks/react-uswds'
+import { Button } from '@trussworks/react-uswds'
 import { PageLayout } from 'components/PageLayout/PageLayout'
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { object, string } from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
-import { SubmitHandler, useForm } from 'react-hook-form'
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form'
+import TextField from 'components/form/fields/TextField/TextField'
+import { getFormattedSsn } from 'utils/format'
 
 type SSNValues = {
   ssn: string
@@ -19,60 +21,53 @@ const SSNInput = () => {
 
   const schema = object().shape({
     ssn: string()
-      .matches(/^[0-9]{3}-?[0-9]{2}-?[0-9]{4}$/, t('errors.badFormat'))
       .required(t('errors.required'))
-      // TODO: add validation from ss guidance
-      // https://secure.ssa.gov/poms.nsf/lnx/0110201035
-      // regex: https://uibakery.io/regex-library/ssn
+      .matches(/^[0-9]{3}-?[0-9]{2}-?[0-9]{4}$/, t('errors.badFormat'))
       .test('ssn', t('errors.invalid'), (value) =>
-        value
-          ? /^(?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4}$/.test(value)
-          : false
+        /^(?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4}$/.test(
+          getFormattedSsn(value)
+        )
       ),
   })
 
-  const {
-    handleSubmit,
-    formState: { errors },
-  } = useForm<SSNValues>({
+  const hookFormMethods = useForm<SSNValues>({
     defaultValues,
     resolver: yupResolver(schema),
   })
+  const { handleSubmit } = hookFormMethods
 
   const [showSsn, setShowSsn] = useState<boolean>(false)
   const handleToggleSsn = () => {
     setShowSsn(!showSsn)
   }
-  const onSubmit: SubmitHandler<SSNValues> = (data) => console.log(data)
+  const onSubmit: SubmitHandler<SSNValues> = (data) => {
+    console.log(data)
+  }
 
   return (
     <PageLayout heading={t('header')}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="margin-bottom-1">
-          {/* TODO: replace with new TextField component */}
-          <Label htmlFor="ssn">{t('label')}</Label>
-          <ErrorMessage>{errors.ssn?.message}</ErrorMessage>
-          <TextInput
-            id="ssn"
-            name="ssn"
-            type={showSsn ? 'text' : 'password'}
-            maxLength={11}
-            minLength={9}
-          />
-          {/* end TODO */}
-          <div className="text-right maxw-mobile-lg">
-            <button
-              className="usa-button usa-button--unstyled text-right"
-              type="button"
-              data-testid="toggleShowNumber"
-              onClick={handleToggleSsn}
-            >
-              {showSsn ? t('hideSsn') : t('showSsn')}
-            </button>
+      <FormProvider {...hookFormMethods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="margin-bottom-1">
+            <TextField
+              label={t('label')}
+              name="ssn"
+              type={showSsn ? 'text' : 'password'}
+            />
+            <div className="text-right maxw-mobile-lg">
+              <button
+                className="usa-button usa-button--unstyled text-right"
+                type="button"
+                data-testid="toggleShowNumber"
+                onClick={handleToggleSsn}
+              >
+                {showSsn ? t('hideSsn') : t('showSsn')}
+              </button>
+            </div>
           </div>
-        </div>
-        <Button type="submit">{t('continue')}</Button>
-      </form>
+          <Button type="submit">{t('continue')}</Button>
+        </form>
+      </FormProvider>
     </PageLayout>
   )
 }
