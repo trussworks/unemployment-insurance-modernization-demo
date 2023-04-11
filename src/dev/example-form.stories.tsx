a0ea6acfeb95b5107f44c0ebf807552ebe78c2c1
Brandon Lenz <brandonalenz@gmail.com>
2023-04-11 10:37:16 -0400

    Use Radio field in example form story

--- src/dev/example-form.stories.tsx
+++ src/dev/example-form.stories.tsx
@@ -1,8 +1,14 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { SubmitHandler, useForm } from 'react-hook-form'
+import {
+  FormProvider,
+  SubmitErrorHandler,
+  SubmitHandler,
+  useForm,
+} from 'react-hook-form'
 import { ChangeEventHandler } from 'react'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
+import { RadioField } from 'components/form/fields/RadioField/RadioField'
 
 const formLibraryPreferenceOptions = ['formik', 'reactHookForm'] as const
 type FormLibraryPreferenceOption = (typeof formLibraryPreferenceOptions)[number]
@@ -29,80 +35,67 @@ const defaultValues: ExampleFieldValues = {
   whyIsFormikBad: undefined,
 }
 const ExampleForm = () => {
+  const hookFormMethods = useForm<ExampleFieldValues>({
+    defaultValues,
+    resolver: yupResolver(schema),
+  })
   const {
     register,
     handleSubmit,
     watch,
     setValue,
     formState: { errors },
-  } = useForm<ExampleFieldValues>({
-    defaultValues,
-    resolver: yupResolver(schema),
-  })
+  } = hookFormMethods
 
   const formLibraryPreference = watch('formLibraryPreference')
+  console.log({ formLibraryPreference })
 
   const onSubmit: SubmitHandler<ExampleFieldValues> = (data) =>
     console.log(data)
 
-  const { onChange, ...remainingProps } = register('formLibraryPreference')
+  const onSubmitError: SubmitErrorHandler<ExampleFieldValues> = (errors) =>
+    console.log(errors)
+
   const handleFormLibraryPreferenceChange: ChangeEventHandler<
     HTMLInputElement
   > = async (e) => {
-    await onChange(e)
+    console.log('CALLING CUSTOM ON CHANGE')
     if (e.target.value === 'formik') {
       setValue('whyIsFormikBad', defaultValues.whyIsFormikBad)
     }
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <fieldset>
-        <legend>Which form Library is better?</legend>
-        <span className="usa-error-message" role="alert">
-          {errors.formLibraryPreference?.message}
-        </span>
-        <div>
-          <input
-            {...remainingProps}
-            id="formLibraryPreference.Formik"
-            type="radio"
-            value="formik"
-            onChange={handleFormLibraryPreferenceChange}
-          />
-          <label htmlFor="formLibraryPreference.Formik">Formik</label>
-        </div>
-        <div>
-          <input
-            {...remainingProps}
-            id="formLibraryPreference.reactHookForm"
-            type="radio"
-            value="reactHookForm"
-            onChange={handleFormLibraryPreferenceChange}
-          />
-          <label htmlFor="formLibraryPreference.reactHookForm">
-            React Hook Form
-          </label>
-        </div>
-      </fieldset>
+    <FormProvider {...hookFormMethods}>
+      <form onSubmit={handleSubmit(onSubmit, onSubmitError)}>
+        <RadioField
+          name="formLibraryPreference"
+          legend="Which form Library is better?"
+          options={formLibraryPreferenceOptions.map((option) => ({
+            label: option, // TODO i18n
+            value: option,
+          }))}
+          onChange={handleFormLibraryPreferenceChange}
+        />
 
-      <br />
+        <br />
 
-      {formLibraryPreference && formLibraryPreference !== 'formik' && (
-        <>
-          <div>
-            <label htmlFor="whyIsFormikBad">Why is Formik bad?</label>
-            <span className="usa-error-message" role="alert">
-              {errors.whyIsFormikBad?.message}
-            </span>
-            <input {...register('whyIsFormikBad')} />
-          </div>
-          <br />
-        </>
-      )}
+        {formLibraryPreference && formLibraryPreference !== 'formik' && (
+          <>
+            <div>
+              <label htmlFor="whyIsFormikBad">Why is Formik bad?</label>
+              <span className="usa-error-message" role="alert">
+                {errors.whyIsFormikBad?.message}
+              </span>
+              <input {...register('whyIsFormikBad')} />
+            </div>
+            <br />
+          </>
+        )}
 
-      <button type="submit">Submit</button>
-    </form>
+        <button type="submit">Submit</button>
+      </form>
+    </FormProvider>
   )
 }
 
