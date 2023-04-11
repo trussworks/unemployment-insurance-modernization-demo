5e6b08eb17fabcc40ea6645b3980ceb91eefdada
Brandon Lenz <brandonalenz@gmail.com>
2023-04-11 10:37:00 -0400

    Add RadioField

--- custom.d.ts
+++ custom.d.ts
@@ -7,3 +7,5 @@ declare module '*.svg' {
   const content: any
   export default content
 }
+
+type Optional<T, K extends keyof T> = Partial<T> & Omit<T, K>
---
+++ src/components/form/fields/RadioField/RadioField.tsx
@@ -0,0 +1,113 @@
+import {
+  Radio,
+  FormGroup,
+  ErrorMessage,
+  Fieldset,
+} from '@trussworks/react-uswds'
+import {
+  ChangeEventHandler,
+  ComponentProps,
+  FocusEventHandler,
+  ReactNode,
+} from 'react'
+
+import styles from './RadioField.module.scss'
+
+import { useController, useFormContext } from 'react-hook-form'
+
+interface IRadioOption {
+  label: ReactNode
+  value: string
+  labelDescription?: string
+}
+
+type RadioInputProps = Optional<
+  Omit<ComponentProps<typeof Radio>, 'label' | 'value'>,
+  'id'
+>
+
+interface IRadioFieldProps extends RadioInputProps {
+  options: IRadioOption[]
+  errorMessage?: string
+  showsErrors?: boolean
+  legend?: ReactNode
+  fieldsetClassName?: string
+  hint?: ReactNode
+}
+
+export const RadioField = ({
+  id: idProp,
+  name,
+  options,
+  onChange,
+  onBlur,
+  legend,
+  fieldsetClassName,
+  showsErrors = true,
+  hint,
+  ...inputProps
+}: IRadioFieldProps & JSX.IntrinsicElements['input']) => {
+  const { register } = useFormContext()
+
+  const {
+    field: {
+      onChange: hookFormOnChange,
+      onBlur: hookFormOnBlur,
+      ref,
+      value: ignored,
+      ...hookFormRemainingProps
+    },
+    fieldState: { invalid, error },
+  } = useController({ name })
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = async (e) => {
+    console.log('CALLING HANDLE CHANGE', e)
+    await hookFormOnChange(e)
+    if (onChange) {
+      onChange(e)
+    }
+  }
+
+  const handleBlur: FocusEventHandler<HTMLInputElement> = (e) => {
+    hookFormOnBlur()
+    if (onBlur) {
+      onBlur(e)
+    }
+  }
+
+  const id = idProp || name
+
+  return (
+    <FormGroup error={!!error}>
+      <Fieldset
+        legend={legend}
+        className={`${fieldsetClassName} ${styles.fieldsetLegend} ${
+          error && styles.errorLegend
+        }`}
+        onInvalid={(e) => e.preventDefault()}
+      >
+        {hint && (
+          <span className="usa-hint" id={`${name}.hint`}>
+            {hint}
+          </span>
+        )}
+        {invalid && <ErrorMessage>{error?.message}</ErrorMessage>}
+        {options.map((option, index) => (
+          <Radio
+            key={`${id}.${index}.${option.value}`}
+            id={`${id}.${option.value}`}
+            data-testid={`${id}.${option.value}`}
+            label={option.label}
+            labelDescription={option?.labelDescription}
+            value={option.value}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            inputRef={ref}
+            {...inputProps}
+            {...hookFormRemainingProps}
+          />
+        ))}
+      </Fieldset>
+    </FormGroup>
+  )
+}
