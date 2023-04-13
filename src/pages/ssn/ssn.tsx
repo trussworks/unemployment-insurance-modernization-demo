919cee8ce4fdc204c53254177c6558c9e8ccf26d
Brandon Lenz <brandonalenz@gmail.com>
2023-04-13 18:13:28 -0400

    Move validation schema outside component

--- src/pages/ssn/ssn.tsx
+++ src/pages/ssn/ssn.tsx
@@ -2,6 +2,7 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import { Button } from '@trussworks/react-uswds'
 import TextField from 'components/form/fields/TextField/TextField'
 import { PageLayout } from 'components/PageLayout/PageLayout'
+import i18n from 'i18n/i18n'
 import { useState } from 'react'
 import { FormProvider, SubmitHandler, useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
@@ -15,27 +16,28 @@ const defaultValues: SSNValues = {
   ssn: '',
 }
 
+const tSsn = i18n.getFixedT(null, 'pages', 'ssn')
+
+const validationSchema = object().shape({
+  ssn: string()
+    .required(tSsn('questions.ssn.errors.required'))
+    .matches(
+      /^[0-9]{3}-?[0-9]{2}-?[0-9]{4}$/,
+      tSsn('questions.ssn.errors.badFormat')
+    )
+    .test('ssn', tSsn('questions.ssn.errors.invalid'), (value) =>
+      /^(?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4}$/.test(
+        getFormattedSsn(value)
+      )
+    ),
+})
+
 export const SSN = () => {
   const { t } = useTranslation('pages', { keyPrefix: 'ssn' })
 
-  // TODO move outside component render function
-  const schema = object().shape({
-    ssn: string()
-      .required(t('questions.ssn.errors.required'))
-      .matches(
-        /^[0-9]{3}-?[0-9]{2}-?[0-9]{4}$/,
-        t('questions.ssn.errors.badFormat')
-      )
-      .test('ssn', t('questions.ssn.errors.invalid'), (value) =>
-        /^(?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4}$/.test(
-          getFormattedSsn(value)
-        )
-      ),
-  })
-
   const hookFormMethods = useForm<SSNValues>({
     defaultValues,
-    resolver: yupResolver(schema),
+    resolver: yupResolver(validationSchema),
   })
   const { handleSubmit } = hookFormMethods
 
