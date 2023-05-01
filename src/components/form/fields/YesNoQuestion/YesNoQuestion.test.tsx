860ed9d5c026952a921dc92e7a9414b83138a690
Joe Kleinschmidt <joekleinschmidt@truss.works>
2023-05-01 10:55:19 -0500

    Add github action for unit test

--- src/components/form/fields/YesNoQuestion/YesNoQuestion.test.tsx
+++ src/components/form/fields/YesNoQuestion/YesNoQuestion.test.tsx
@@ -1,18 +1,28 @@
 import { render } from '@testing-library/react'
 import { YesNoQuestion } from 'components/form/fields/YesNoQuestion/YesNoQuestion'
 import { ComponentProps } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
 
 describe('YesNoQuestion', () => {
+  const CustomComponent = (
+    props?: Omit<ComponentProps<typeof YesNoQuestion>, 'name' | 'question'>
+  ) => {
+    const methods = useForm()
+
+    return (
+      <FormProvider {...methods}>
+        <YesNoQuestion
+          name="question"
+          question="What is the answer?"
+          {...props}
+        />
+      </FormProvider>
+    )
+  }
   const renderYesNoQuestion = (
     props?: Omit<ComponentProps<typeof YesNoQuestion>, 'name' | 'question'>
   ) => {
-    render(
-      <YesNoQuestion
-        name="question"
-        question="What is the answer?"
-        {...props}
-      />
-    )
+    render(<CustomComponent {...props} />)
     return {}
   }
 
