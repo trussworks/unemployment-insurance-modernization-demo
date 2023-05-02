1012d99d721c54e2529e919224b2cdd2f8c664a9
brandonlenz <brandonalenz@gmail.com>
2023-05-02 16:16:08 -0400

    Set up coverage

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,13 @@ import type { Config } from 'jest'
 
 const config: Config = {
   rootDir: 'src',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/i18n/**',
+    '!**/*.stories.*',
+  ],
+  coverageDirectory: 'coverage/jest',
   moduleDirectories: ['node_modules', '<rootDir>'],
   transform: {
     '\\.[jt]sx?$': 'babel-jest',
