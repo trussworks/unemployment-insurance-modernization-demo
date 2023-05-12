c034f86d04413e70957cc031f2e971b5fde744a0
Joe Kleinschmidt <joekleinschmidt@truss.works>
2023-05-12 10:25:43 -0500

    Add test for toggling menu

--- src/components/CustomHeader/CustomHeader.test.tsx
+++ src/components/CustomHeader/CustomHeader.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen, within } from '@testing-library/react'
+import { act, render, screen, within } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import { CustomHeader } from './CustomHeader'
 
@@ -6,6 +7,7 @@ const HEADER_TEXT = 'Unemployment Insurance Benefits'
 const HOME = 'Home'
 const MY_CLAIM = 'My claim'
 const LOG_OUT = 'Log out'
+const MENU = 'Menu'
 
 describe('CustomHeader', () => {
   const renderCustomHeader = () => {
@@ -16,6 +18,7 @@ describe('CustomHeader', () => {
     const home = within(nav).getByRole('link', { name: HOME })
     const myClaim = within(nav).getByRole('link', { name: MY_CLAIM })
     const logOut = within(nav).getByRole('link', { name: LOG_OUT })
+    const menu = screen.getByRole('button', { name: MENU })
 
     return {
       header,
@@ -23,11 +26,12 @@ describe('CustomHeader', () => {
       home,
       myClaim,
       logOut,
+      menu,
     }
   }
 
   it('Renders without error', () => {
-    const { header, nav, home, myClaim, logOut } = renderCustomHeader()
+    const { header, nav, home, myClaim, logOut, menu } = renderCustomHeader()
 
     expect(header).toBeInTheDocument()
     expect(header).toHaveTextContent(HEADER_TEXT)
@@ -38,5 +42,22 @@ describe('CustomHeader', () => {
     expect(myClaim).toHaveTextContent(MY_CLAIM)
     expect(logOut).toBeInTheDocument()
     expect(logOut).toHaveTextContent(LOG_OUT)
+    expect(menu).toBeInTheDocument()
+    expect(menu).toHaveTextContent(MENU)
+  })
+
+  it('Toggles the menu', async () => {
+    const user = userEvent.setup()
+
+    const { menu, nav } = renderCustomHeader()
+
+    expect(nav).not.toHaveClass('is-visible')
+
+    await act(() => user.click(menu))
+
+    const overlay = screen.getByTestId('overlay')
+
+    expect(overlay).toBeInTheDocument()
+    expect(nav).toHaveClass('is-visible')
   })
 })
